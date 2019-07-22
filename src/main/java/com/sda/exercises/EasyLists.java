b3a5ef3f232108d941592bed1987200bd165f623
wielki.borsuk <wielki.borsuk@gmail.com>
2019-07-23 01:05:50 +0200

    initial commit, easy collections solution

---
+++ src/main/java/com/sda/exercises/EasyLists.java
@@ -0,0 +1,66 @@
+package com.sda.exercises;
+
+import java.util.*;
+
+public class EasyLists {
+    public static void main(String[] args) {
+
+        words();
+        uniqueWords();
+        sortedWords();
+        sortedUniqueWords();
+    }
+
+    static void words() {
+        Scanner sc = new Scanner(System.in);
+        List<String> words = new ArrayList<String>();
+        String line = sc.nextLine();
+
+        while(!"-".equals(line)) {
+            words.add(line);
+            line = sc.nextLine();
+        }
+
+        System.out.println(words);
+    }
+
+    static void uniqueWords() {
+        Scanner sc = new Scanner(System.in);
+        Set<String> words = new HashSet<String>();
+        String line = sc.nextLine();
+
+        while(!"-".equals(line)) {
+            words.add(line);
+            line = sc.nextLine();
+        }
+
+        System.out.println(words);
+    }
+
+    static void sortedWords() {
+        Scanner sc = new Scanner(System.in);
+        List<String> words = new ArrayList<String>();
+        String line = sc.nextLine();
+
+        while(!"-".equals(line)) {
+            words.add(line);
+            line = sc.nextLine();
+        }
+
+        Collections.sort(words);
+        System.out.println(words);
+    }
+
+    static void sortedUniqueWords() {
+        Scanner sc = new Scanner(System.in);
+        Set<String> words = new TreeSet<String>();
+        String line = sc.nextLine();
+
+        while(!"-".equals(line)) {
+            words.add(line);
+            line = sc.nextLine();
+        }
+
+        System.out.println(words);
+    }
+}
---
+++ src/main/java/com/sda/exercises/EasyMaps.java
@@ -0,0 +1,7 @@
+package com.sda.exercises;
+
+import java.util.*;
+
+public class EasyMaps {
+
+}
