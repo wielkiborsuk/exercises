e4566de8bb908a92e5f07b574ad36cd6bb52475a
wielki.borsuk <wielki.borsuk@gmail.com>
2020-05-01 07:21:59 +0200

    adjust collections scope, load data from files

--- src/main/java/com/sda/exercises/collections/EasyMaps.java
+++ src/main/java/com/sda/exercises/collections/EasyMaps.java
@@ -1,64 +1,69 @@
 package com.sda.exercises.collections;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
 
 import static java.lang.String.format;
 
 public class EasyMaps {
 
-    public static void main(String[] args) {
-//        readUniqueNames(); // use with "pesele.i.nazwiska.txt" file from resources
+    public static void main(String[] args) throws IOException {
+        readUniqueNames(); // use with "pesele.i.nazwiska.txt" file from resources
 //        readDuplicateNames(); // use with "pesele.i.powtorzone.nazwiska.txt" file from resources
-        readCountries(); // use with "kraje.txt" file from resources
+//        readCountries(); // use with "kraje.txt" file from resources
     }
 
-    private static void readUniqueNames() {
-        Scanner sc = new Scanner(System.in);
+    private static void readUniqueNames() throws IOException {
+        String fileName = "src/main/resources/input/maps/pesele.i.nazwiska.txt";
         Map<String, String> people = new HashMap<>();
-        String[] parts = sc.nextLine().split(" ");
 
-        while(!"0".equals(parts[0])) {
-            people.put(parts[0], format("%s %s", parts[1], parts[2]));
-            parts = sc.nextLine().split(" ");
-        }
+        Files.lines(Paths.get(fileName))
+                .forEach(line -> {
+                    String[] parts = line.split(" ");
+                    people.put(parts[0], format("%s %s", parts[1], parts[2]));
+                });
+
+        Scanner sc = new Scanner(System.in);
 
         System.out.println(people.get(sc.nextLine()));
     }
 
-    private static void readDuplicateNames() {
-        Scanner sc = new Scanner(System.in);
+    private static void readDuplicateNames() throws IOException {
+        String fileName = "src/main/resources/input/maps/pesele.i.powtorzone.nazwiska.txt";
         Map<String, List<String>> people = new HashMap<>();
-        String[] parts = sc.nextLine().split(" ");
 
-        while(!"0".equals(parts[0])) {
-            String name = format("%s %s", parts[1], parts[2]);
-            if (!people.containsKey(name)) {
-                people.put(name, new ArrayList<>());
-            }
-            people.get(name).add(parts[0]);
-            parts = sc.nextLine().split(" ");
-        }
+        Files.lines(Paths.get(fileName))
+                .forEach(line -> {
+                    String[] parts = line.split(" ");
+                    String name = format("%s %s", parts[1], parts[2]);
+                    if (!people.containsKey(name)) {
+                        people.put(name, new ArrayList<>());
+                    }
+                    people.get(name).add(parts[0]);
+                });
+
+        Scanner sc = new Scanner(System.in);
 
         System.out.println(people.get(sc.nextLine()));
     }
 
-    private static void readCountries() {
+    private static void readCountries() throws IOException {
+        String fileName = "src/main/resources/input/maps/kraje.txt";
+        Map<String, String> countryToCode = new HashMap<>();
+        Map<String, String> codeToCountry = new HashMap<>();
+
+        Files.lines(Paths.get(fileName))
+                .forEach(line -> {
+                    String[] parts = line.split(" ");
+                    countryToCode.put(parts[0], parts[1]);
+                    codeToCountry.put(parts[1], parts[0]);
+                });
         Scanner sc = new Scanner(System.in);
-        Map<String, String> countries = new HashMap<>();
-        String[] parts = sc.nextLine().split(" ");
 
-        while(!"-".equals(parts[0])) {
-            countries.put(parts[0], parts[1]);
-            parts = sc.nextLine().split(" ");
-        }
-
-        System.out.println(countries.get(sc.nextLine()));
-        String countryCode = sc.nextLine();
-        countries.forEach((key, value) -> {
-            if (countryCode.equals(value)) {
-                System.out.println(key);
-            }
-        });
+        System.out.println(countryToCode.get(sc.nextLine()));
+        System.out.println(codeToCountry.get(sc.nextLine()));
     }
 
 }
--- src/main/java/com/sda/exercises/collections/SmartMaps.java
+++ src/main/java/com/sda/exercises/collections/SmartMaps.java
@@ -5,55 +5,58 @@ import org.apache.commons.collections4.MultiValuedMap;
 import org.apache.commons.collections4.bidimap.TreeBidiMap;
 import org.apache.commons.collections4.multimap.ArrayListValuedHashMap;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
+import java.util.stream.Collectors;
 
 import static java.lang.String.format;
 
 public class SmartMaps {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         readUniqueNames(); // use with "pesele.i.nazwiska.txt" file from resources
 //        readDuplicateNames(); // use with "pesele.i.powtorzone.nazwiska.txt" file from resources
 //        readCountries(); // use with "kraje.txt" file from resources
     }
 
-    private static void readUniqueNames() {
+    private static void readUniqueNames() throws IOException {
+        String fileName = "src/main/resources/input/maps/pesele.i.nazwiska.txt";
+        Map<String, String> people = Files.lines(Paths.get(fileName))
+                .map(line -> line.split(" "))
+                .collect(Collectors.toMap(parts -> parts[0], parts -> format("%s %s", parts[1], parts[2])));
+
         Scanner sc = new Scanner(System.in);
-        Map<String, String> people = new HashMap<>();
-        String[] parts = sc.nextLine().split(" ");
-
-        while(!"0".equals(parts[0])) {
-            people.put(parts[0], format("%s %s", parts[1], parts[2]));
-            parts = sc.nextLine().split(" ");
-        }
-
         System.out.println(people.get(sc.nextLine()));
     }
 
-    private static void readDuplicateNames() {
-        Scanner sc = new Scanner(System.in);
+    private static void readDuplicateNames() throws IOException {
+        String fileName = "src/main/resources/input/maps/pesele.i.pow.nazwiska.txt";
         MultiValuedMap<String, String> people = new ArrayListValuedHashMap<>();
-        String[] parts = sc.nextLine().split(" ");
 
-        while(!"0".equals(parts[0])) {
-            String name = format("%s %s", parts[1], parts[2]);
-            people.put(name, parts[0]);
-            parts = sc.nextLine().split(" ");
-        }
+        Files.lines(Paths.get(fileName))
+                .forEach(line -> {
+                    String[] parts = line.split(" ");
+                    String name = format("%s %s", parts[1], parts[2]);
+                    people.put(name, parts[0]);
+                });
 
+        Scanner sc = new Scanner(System.in);
         System.out.println(people.get(sc.nextLine()));
     }
 
-    private static void readCountries() {
-        Scanner sc = new Scanner(System.in);
+    private static void readCountries() throws IOException {
+        String fileName = "src/main/resources/input/maps/kraje.txt";
         BidiMap<String, String> countries = new TreeBidiMap<>();
-        String[] parts = sc.nextLine().split(" ");
 
-        while(!"-".equals(parts[0])) {
-            countries.put(parts[0], parts[1]);
-            parts = sc.nextLine().split(" ");
-        }
+        Files.lines(Paths.get(fileName))
+                .forEach(line -> {
+                    String[] parts = line.split(" ");
+                    countries.put(parts[0], parts[1]);
+                });
 
+        Scanner sc = new Scanner(System.in);
         System.out.println(countries.get(sc.nextLine()));
         System.out.println(countries.getKey(sc.nextLine()));
     }
